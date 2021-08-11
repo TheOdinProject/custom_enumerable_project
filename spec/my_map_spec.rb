22f12311a2e41f1cbe50c7b96bfce20857382da4
Ranko Liang <rankoliang@gmail.com>
2021-08-11 19:01:26 -0400

    Add my_map spec

---
+++ spec/my_map_spec.rb
@@ -0,0 +1,17 @@
+# frozen_string_literal: true
+
+require_relative '../lib/my_enumerables'
+
+RSpec.describe Enumerable do
+  let(:enumerable) { [1, 1, 2, 3, 5, 8, 13, 21, 34] }
+
+  describe '#my_map' do
+    it 'returns an array with the results of running the given block for every element' do
+      expect(enumerable.my_map { |value| value * 2 }).to eq([2, 2, 4, 6, 10, 16, 26, 42, 68])
+    end
+
+    it 'returns an array with the same size as the enumerable' do
+      expect(enumerable.my_map { |value| value * 2 }.size).to eq enumerable.size
+    end
+  end
+end
