8775554210df2a1573ab1802c7f6236b0b9f9651
Ranko Liang <rankoliang@gmail.com>
2021-09-25 11:53:06 -0400

    Update my_each and my_each_with_index
    Adds tests to ensure that the block is executed and that it returns the
    original array.

--- spec/my_each_spec.rb
+++ spec/my_each_spec.rb
@@ -6,14 +6,30 @@ RSpec.describe Array do
   subject(:array) { [1, 1, 2, 3, 5, 8, 13, 21, 34] }
 
   describe '#my_each' do
-    it 'matches #each' do
-      my_each_results = []
+    context 'when given a block' do
+      it 'returns the original array' do
+        my_each_results = array.my_each do |_element|
+          # This should return the original array
+          # no matter the contents of the block
+        end
 
-      array.my_each do |element|
-        my_each_results << element
+        expect(my_each_results).to eq(array)
       end
 
-      expect(my_each_results).to eq(array.each.to_a)
+      it 'executes the block for each element' do
+        my_each_results = []
+        each_results = []
+
+        array.my_each do |element|
+          my_each_results << element * 2
+        end
+
+        array.each do |element|
+          each_results << element * 2
+        end
+
+        expect(my_each_results).to eq(each_results)
+      end
     end
   end
 end
