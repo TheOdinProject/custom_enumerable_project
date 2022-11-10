84fd186d5f29026287560de7ec6a9061b1853cc2
Jee El <98971420+Jee-El@users.noreply.github.com>
2022-11-10 21:27:14 +0100

    feat: add more tests for #my_inject

--- spec/my_inject_spec.rb
+++ spec/my_inject_spec.rb
@@ -6,23 +6,39 @@ RSpec.describe Enumerable do
   subject(:enumerable) { [1, 1, 2, 3, 5, 8, 13, 21, 34] }
 
   describe '#my_inject' do
-    it 'reduces an enumerable to a single value' do
-      initial_value = 0
-      # calculates the sum of the elements of the enumerable array
-      expect(enumerable.my_inject(initial_value) { |sum, value| sum + value }).to eq 88
+    context 'when it receives a symbol' do
+      it 'combines the operands using the method' do
+        expect(enumerable.my_inject(:-)).to eq -86
+      end
     end
 
-    it 'can be used to calculate the product' do
-      product = enumerable.my_inject(1) { |prod, value| prod * value }
-
-      expect(product).to eq 2_227_680
+    context 'when it receives an initial_operand and a symbol' do
+      it 'uses the initial value on the first iteration and combines the operands using the method' do
+        expect(enumerable.my_inject(88, :-)).to be_zero
+      end
     end
 
-    it 'uses the initial value on the first iteration' do
-      initial_value = 100
+    context 'when it receives a block' do
+      it 'passes each operand to the block' do
+        expect(enumerable.my_inject { |memo, operand| memo + operand }).to eq 88
+      end
+    end
 
-      # calculates the sum of the elements of the enumerable array plus the initial value
-      expect(enumerable.my_inject(initial_value) { |sum, value| sum + value }).to eq 188
+    context 'when it receives an initial_operand and a block' do
+      it 'sums the elements of the enumerable array' do
+        expect(enumerable.my_inject(0) { |sum, value| sum + value }).to eq 88
+      end
+  
+      it 'multiplies the elements of the enumerable array' do
+        product = enumerable.my_inject(1) { |prod, value| prod * value }
+  
+        expect(product).to eq 2_227_680
+      end
+  
+      it 'uses the initial value on the first iteration' do
+        # calculates the sum of the elements of the enumerable array plus the initial value
+        expect(enumerable.my_inject(100) { |sum, value| sum + value }).to eq 188
+      end
     end
   end
 end
