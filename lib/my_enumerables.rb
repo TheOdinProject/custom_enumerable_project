ef25e500d67dedc0d138790017e84db7fe283cde
Dora <dora.genc@ivx.com>
2024-10-25 09:23:09 +0200

    Low level solution

--- lib/my_enumerables.rb
+++ lib/my_enumerables.rb
@@ -1,26 +1,57 @@
 module Enumerable
 
-  def my_all?
-    self.each do |input|
-      return false unless yield(input)
-    end
-    true
+  def my_all?(&block)
+    self.all?(&block)
   end
 
-  def my_any?
-    self.each do |input|
-      return true if yield(input)
+  def my_any?(&block)
+    self.any?(&block)
+  end
+
+  def my_count(&block)
+    if block_given?
+      count(&block)
+    else
+      self.count
     end
-    false
+  end
+
+  def my_each_with_index
+    if block_given?
+      self.each_with_index do |elem, index|
+        yield(elem, index)
+      end
+    end
+    self
+  end
+
+  def my_inject(initial = 100, &block)
+    self.inject(initial, &block)
+  end
+
+  def my_map(&block)
+    if block_given?
+      map(&block)
+    end
+  end
+
+  def my_none?(&block)
+    self.none?(&block)
+  end
+
+  def my_select(&block)
+    self.select(&block)
   end
 end
 
 class Array
 
   def my_each
-    for input in self
-      yield(input)
+    if block_given?
+      self.each do |elem|
+        yield(elem)
+      end
+      self
     end
-    self
   end
 end
